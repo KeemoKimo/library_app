d6c521a0d24b2b4a94d4fd9c955f88d4838b78cc
kongbunthong <thebunthong@gmail.com>
2021-08-10 08:19:39 +0700

    Update User Data
    Now able to update the user data from the app , and change it in the firebase database.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:library_app/DatabaseSerivces.dart';
+import 'package:library_app/main.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,21 +14,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  late String username;
   late User loggedInUser;
+  late String? userEmail = loggedInUser.email;
+  late String username;
+  late String age;
 
   @override
   void initState() {
     super.initState();
-    getCurrentUser();
+    getCurrentUser().whenComplete(() {
+      setState(() {
+        HomeScreen();
+      });
+    });
   }
 
-  void getCurrentUser() async {
+  getCurrentUser() async {
     try {
       final user = await auth.currentUser;
       if (user != null) {
         loggedInUser = user;
-        print(loggedInUser.email);
       }
     } catch (e) {
       print(
@@ -51,20 +58,195 @@ class _HomeScreenState extends State<HomeScreen> {
                 decoration: BoxDecoration(
                   color: Colors.blue,
                 ),
-                child: Text('Drawer Header'),
+                child: Center(
+                  child: Text('$userEmail'),
+                ),
               ),
               ListTile(
-                title: const Text('Item 1'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
+                leading: Icon(Icons.settings),
+                title: const Text('Edit profile'),
+                onTap: () async {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return SafeArea(
+                          child: Container(
+                              padding: EdgeInsets.only(left: 20, right: 20),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(top: 20),
+                                    child: Text(
+                                      "USERNAME : ",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 20),
+                                    padding:
+                                        EdgeInsets.only(left: 20, right: 20),
+                                    child: TextFormField(
+                                      style: TextStyle(color: Colors.black),
+                                      decoration: InputDecoration(
+                                        labelText: "SET USERNAME...",
+                                        labelStyle:
+                                            TextStyle(color: Colors.black),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(15.0),
+                                          borderSide: BorderSide(
+                                            color: Colors.black,
+                                            width: 2.0,
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(25.0),
+                                          borderSide: BorderSide(
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                      ),
+                                      onChanged: (value) {
+                                        username = value;
+                                      },
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please enter some text';
+                                        }
+                                        return null;
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 20),
+                                    child: Text(
+                                      "Age : ",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 20),
+                                    padding:
+                                        EdgeInsets.only(left: 20, right: 20),
+                                    child: TextFormField(
+                                      style: TextStyle(color: Colors.black),
+                                      decoration: InputDecoration(
+                                        labelText: "AGE...",
+                                        labelStyle:
+                                            TextStyle(color: Colors.black),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(15.0),
+                                          borderSide: BorderSide(
+                                            color: Colors.black,
+                                            width: 2.0,
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(25.0),
+                                          borderSide: BorderSide(
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                      ),
+                                      onChanged: (value) {
+                                        age = value;
+                                      },
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please enter some text';
+                                        }
+                                        return null;
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 20),
+                                    color: Colors.yellow[800],
+                                    width: 300,
+                                    height: 70,
+                                    child: TextButton(
+                                      onPressed: () async {
+                                        try {
+                                          DatabaseServices(
+                                                  uid: loggedInUser.uid)
+                                              .updateUserData(
+                                                  username, age, userEmail!);
+                                          print("Edited User");
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) {
+                                                return AlertDialog(
+                                                  content: Container(
+                                                    color: Colors.green,
+                                                    child: Text(
+                                                        "PROFILE CHANGED !!!"),
+                                                  ),
+                                                );
+                                              });
+                                        } catch (e) {
+                                          print(e.toString());
+                                        }
+                                      },
+                                      child: Text(
+                                        "CONFIRM CHANGE",
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 15),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin:
+                                        EdgeInsets.only(top: 20, bottom: 20),
+                                    child: Divider(
+                                      thickness: 1,
+                                      height: 1,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 20),
+                                    child: Text(
+                                      "CURRENT USER : $userEmail",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )),
+                        );
+                      });
                 },
               ),
               ListTile(
-                title: const Text('Item 2'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
+                leading: Icon(Icons.favorite),
+                title: const Text('Favourites'),
+                onTap: () {},
+              ),
+              Divider(
+                height: 1,
+                thickness: 1,
+                color: Colors.black,
+              ),
+              ListTile(
+                leading: Icon(Icons.logout_outlined),
+                title: const Text(
+                  'Log Out',
+                ),
+                onTap: () async {
+                  await auth.signOut();
+                  print("Signed Out successful");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
                 },
               ),
             ],
