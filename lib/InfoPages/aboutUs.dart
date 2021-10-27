77e7c81d78f38760d4d57f291e61621e5b744d3e
kongbunthong <thebunthong@gmail.com>
2021-10-27 08:19:32 +0700

    Meet the team - About us page
    Added the "Meet the team" section inside About us page.
    
    5 people, all of them are imaginary.
    
    Infographic profile picture, with carefully selected name according to their country.

--- lib/InfoPages/aboutUs.dart
+++ lib/InfoPages/aboutUs.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'dart:io';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
@@ -39,7 +40,6 @@ Container customDivider(Color color) {
 
 Container rowItem(String imagePath, String firstLineText, String description) {
   return Container(
-    color: Colors.green,
     child: Column(
       children: [
         Container(
@@ -63,14 +63,86 @@ Container rowItem(String imagePath, String firstLineText, String description) {
         ),
         Center(
           child: Container(
-            color: Colors.blue,
             width: 300,
             margin: EdgeInsets.only(top: 20),
             padding: EdgeInsets.all(10),
             child: Text(
               description,
-              style: TextStyle(wordSpacing: 2, fontSize: 15),
+              style: TextStyle(wordSpacing: 3, fontSize: 15, letterSpacing: 2),
             ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Container meetTheTeamContainer(String imagePath, String personName,
+    String personDescription, double marginValue, String flagPath) {
+  return Container(
+    margin: EdgeInsets.only(top: 20),
+    child: Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          width: double.infinity,
+          height: 130,
+          child: Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(5),
+                width: 140,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 3, color: Colors.black),
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: AssetImage(imagePath)),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                width: 190,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 30),
+                      child: Row(
+                        children: [
+                          Text(
+                            personName,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: marginValue),
+                            width: 25,
+                            height: 25,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(5),
+                                ),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage(flagPath),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+                      child: Text(
+                        personDescription,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ],
@@ -108,27 +180,96 @@ class _AboutUsState extends State<AboutUs> {
                     ),
                   ),
                   customDivider(Colors.white),
-                  Text(
-                    'Get to know us',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    child: Text(
+                      'Get to know us',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
                   ),
                   Container(
-                    margin: EdgeInsets.only(top: 20),
+                    margin: EdgeInsets.only(top: 50),
                     padding: EdgeInsets.only(left: 20),
                     width: double.infinity,
                     height: 550,
-                    color: Colors.red,
                     child: SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
                       child: Row(
                         children: [
                           rowItem('assets/images/dev.png', 'Development',
-                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam')
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'),
+                          Container(
+                            margin: EdgeInsets.only(left: 50),
+                            child: rowItem('assets/images/team.png', 'Our Team',
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 50),
+                            child: rowItem(
+                                'assets/images/vision.png',
+                                'Our Vision',
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 50),
+                            child: rowItem(
+                                'assets/images/service.png',
+                                'Service',
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'),
+                          ),
                         ],
                       ),
+                    ),
+                  ),
+                  customDivider(Colors.white),
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    child: Text(
+                      'Meet the team',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 50),
+                    child: Column(
+                      children: [
+                        meetTheTeamContainer(
+                            'assets/images/CEO.png',
+                            'Jürgen Walter',
+                            'CEO & Founder',
+                            20,
+                            'assets/images/germany.png'),
+                        meetTheTeamContainer(
+                            'assets/images/Co-Founder.png',
+                            'Dromos Alexios',
+                            'Co-Founder',
+                            10,
+                            'assets/images/greece.png'),
+                        meetTheTeamContainer(
+                            'assets/images/manager.png',
+                            'Ingrid Anastasia',
+                            'Manager',
+                            10,
+                            'assets/images/norway.png'),
+                        meetTheTeamContainer(
+                            'assets/images/developer.png',
+                            'Bjørn Oddvaren',
+                            'Developer',
+                            15,
+                            'assets/images/denmark.png'),
+                        meetTheTeamContainer(
+                            'assets/images/developer2.png',
+                            'E.Olivia Sinclair',
+                            'Developer',
+                            20,
+                            'assets/images/canada.png'),
+                      ],
                     ),
                   ),
                 ],
