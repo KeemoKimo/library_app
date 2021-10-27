39d98380740108dd1164cb13e26d082cc144c865
kongbunthong <thebunthong@gmail.com>
2021-10-28 06:51:46 +0700

    User can send email to the company
    User now able to send email by entering their email, name, subject, and type in some messages. Message will automatically go to thebunthong@gmail.com
    
    Used the service emailJS.

--- lib/InfoPages/aboutUs.dart
+++ lib/InfoPages/aboutUs.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
 import 'package:animated_text_kit/animated_text_kit.dart';
@@ -19,12 +20,55 @@ import 'package:library_app/categoryPages/philosophy.dart';
 import 'package:library_app/categoryPages/sci_fi.dart';
 import 'package:library_app/main.dart';
 import 'dart:math' as math;
+import 'package:http/http.dart' as http;
 
 class AboutUs extends StatefulWidget {
   const AboutUs({Key? key}) : super(key: key);
 
   @override
   _AboutUsState createState() => _AboutUsState();
+}
+
+var nameController = TextEditingController();
+var emailController = TextEditingController();
+var subjectController = TextEditingController();
+var messageController = TextEditingController();
+
+Future sendEmail({
+  required String name,
+  required String email,
+  required String subject,
+  required String message,
+}) async {
+  final serviceId = 'service_c5gaghb';
+  final templateId = 'template_gwixyuv';
+  final userId = 'user_K1O3yuCrqJUEDru3j777v';
+  final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+  try {
+    final response = await http.post(
+      url,
+      headers: {
+        'origin': 'http://localhost',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(
+        {
+          'service_id': serviceId,
+          'template_id': templateId,
+          'user_id': userId,
+          'template_params': {
+            'name_sender': name,
+            'email_sender': email,
+            'user_subject': subject,
+            'message': message,
+          },
+        },
+      ),
+    );
+    print('Email sent');
+  } catch (e) {
+    print(e);
+  }
 }
 
 Container customDivider(Color color) {
@@ -95,7 +139,7 @@ Container meetTheTeamContainer(String imagePath, String personName,
                 child: Container(
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    border: Border.all(width: 3, color: Colors.black),
+                    border: Border.all(width: 3, color: Colors.white),
                     image: DecorationImage(
                         fit: BoxFit.cover, image: AssetImage(imagePath)),
                   ),
@@ -269,6 +313,188 @@ class _AboutUsState extends State<AboutUs> {
                             'Developer',
                             20,
                             'assets/images/canada.png'),
+                      ],
+                    ),
+                  ),
+                  customDivider(Colors.white),
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    child: Text(
+                      "Get in touch!!!",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(20),
+                    width: double.infinity,
+                    height: 630,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          child: TextFormField(
+                            controller: nameController,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            decoration: InputDecoration(
+                              labelText: "Enter your name...",
+                              labelStyle: TextStyle(color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                  width: 2.0,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          child: TextFormField(
+                            controller: emailController,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            decoration: InputDecoration(
+                              labelText: "Enter your email...",
+                              labelStyle: TextStyle(color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                  width: 2.0,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          child: TextFormField(
+                            controller: subjectController,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            decoration: InputDecoration(
+                              labelText: "Enter the subject...",
+                              labelStyle: TextStyle(color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                  width: 2.0,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          child: TextFormField(
+                            maxLength: 1000,
+                            controller: messageController,
+                            maxLines: 10,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                              counterStyle: TextStyle(color: Colors.black),
+                              labelText: "Write your message...",
+                              labelStyle: TextStyle(color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                  width: 2.0,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          width: 200,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.black,
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              sendEmail(
+                                name: nameController.text,
+                                email: emailController.text,
+                                subject: subjectController.text,
+                                message: messageController.text,
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(left: 60, right: 10),
+                                  child: Text(
+                                    "Send!",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: 32,
+                                  height: 32,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                          'assets/images/sending.png'),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   ),
